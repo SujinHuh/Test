ed40ab0e0da624c173b2b6b76beb474d55923771
SujinHuh <juin483@naver.com>
2022-04-26 12:37:29 +0900

    Feat funtion

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -30,5 +30,41 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, BtsActivity02::class.java)
             startActivity(intent)
         }
+
+        val image3 = findViewById<ImageView>(R.id.image_3) //view image2 가져옴
+        image3.setOnClickListener {
+            Toast.makeText(this, "3번클릭", Toast.LENGTH_LONG).show()
+
+            //2.화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌
+            val intent = Intent(this, BtsActivity03::class.java)
+            startActivity(intent)
+        }
+
+        val image4 = findViewById<ImageView>(R.id.image_4) //view image2 가져옴
+        image4.setOnClickListener {
+            Toast.makeText(this, "4번클릭", Toast.LENGTH_LONG).show()
+
+            //2.화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌
+            val intent = Intent(this, BtsActivity04::class.java)
+            startActivity(intent)
+        }
+
+        val image5 = findViewById<ImageView>(R.id.image_5) //view image2 가져옴
+        image5.setOnClickListener {
+            Toast.makeText(this, "5번클릭", Toast.LENGTH_LONG).show()
+
+            //2.화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌
+            val intent = Intent(this, BtsActivity05::class.java)
+            startActivity(intent)
+        }
+
+        val image6 = findViewById<ImageView>(R.id.image_6) //view image2 가져옴
+        image6.setOnClickListener {
+            Toast.makeText(this, "6번클릭", Toast.LENGTH_LONG).show()
+
+            //2.화면이 클릭되면, 다음 화면으로 넘어가서, 사진을 크게 보여줌
+            val intent = Intent(this, BtsActivity06::class.java)
+            startActivity(intent)
+        }
     }
 }
